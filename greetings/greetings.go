fc4bb614b01358011c59b27e6d317f35558ae371
Ishan Garg <igarg2001@gmail.com>
2023-01-14 11:11:57 +0530

    return a random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,15 +1,31 @@
 package greetings
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
+	"math/rand"
+	"time"
 )
+
 //In Go, a function that begins with a capital letter is an exported name, i.e., it can be used in other packages
 func Hello (name string) (string, error) {
 	//var message string <-------------- only declaration
 	if name == "" {
 		return "", errors.New("empty name") //return both return values, in error case return empty string
 	}
-	message:= fmt.Sprintf("Hi, %v. Welcome!", name) //declaration and initalization
+	message:= fmt.Sprintf(randomFormat(), name) //declaration and initalization
 	return message, nil //return both return values, in correct case return nil error
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats:= []string{
+		"Hi, %v. Welcome",
+		"Great to see you, %v!",
+		"Hail, %v. Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
