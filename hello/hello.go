1bafe2138728e2ee08f5447422b88c06b5e0a7ff
Ishan Garg <igarg2001@gmail.com>
2023-01-14 11:21:42 +0530

    4. return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+
 	"example.com/greetings"
 )
 
@@ -12,9 +13,17 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Ishan")
+	names:=[]string{"John", "Alex", "Samantha"}
+
+	// message, err := greetings.Hello("Ishan")
+	// if err!=nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(message)
+
+	messages, err := greetings.Hellos(names)
 	if err!=nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
